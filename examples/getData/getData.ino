db62ff0059fa2ab3d69d65a63b3cb9e462fe44d0
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-07-14 16:32:41 +0800

    Update getData.ino

--- examples/getData/getData.ino
+++ examples/getData/getData.ino
@@ -1,6 +1,6 @@
 /*!
  *@file getData.ino
- *@brief 读取环境温度和相对湿度,并打印到串口
+ *@brief Read ambient temperature and relative humidity and print them to serial port.
  *@copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  *@licence     The MIT License (MIT)
  *@author [fengli](li.feng@dfrobot.com)
@@ -20,17 +20,17 @@ DFRobot_DHT20 dht20;
 void setup(){
 
   Serial.begin(115200);
-  //传感器初始化
+  //Initialize sensor
   while(dht20.begin()){
-    Serial.println("传感器初始化失败");
+    Serial.println("Initialize sensor failed");
     delay(1000);
   }
 }
 
 void loop(){
-  //获取环境温度
+  //Get ambient temperature
   Serial.print("temperature:"); Serial.print(dht20.getTemperature());Serial.print("C");
-  //获取相对湿度
+  //Get relative humidity
   Serial.print("  humidity:"); Serial.print(dht20.getHumidity()*100);Serial.println(" %RH");
   
   delay(1000);
